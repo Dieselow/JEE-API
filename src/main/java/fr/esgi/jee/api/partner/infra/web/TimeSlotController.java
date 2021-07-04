22ec52a71637f5f2671b4e0a9c2f08a72e51ff4b
Juliien <julien1105@outlook.fr>
2021-07-04 23:09:23 +0200

    feat: crud on reservation

--- src/main/java/fr/esgi/jee/api/partner/infra/web/TimeSlotController.java
+++ src/main/java/fr/esgi/jee/api/partner/infra/web/TimeSlotController.java
@@ -2,34 +2,25 @@ package fr.esgi.jee.api.partner.infra.web;
 
 import fr.esgi.jee.api.geolocaliztion.GeolocaliztionService;
 import fr.esgi.jee.api.geolocaliztion.models.GeoCoordsResponse;
-import fr.esgi.jee.api.partner.domain.PartnerServiceImpl;
 import fr.esgi.jee.api.partner.domain.reservation.Reservation;
 import fr.esgi.jee.api.partner.domain.reservation.ReservationServiceImpl;
 import fr.esgi.jee.api.partner.domain.timeslot.TimeSlot;
 import fr.esgi.jee.api.partner.domain.timeslot.TimeSlotServiceImpl;
-import fr.esgi.jee.api.users.domain.User;
-import fr.esgi.jee.api.users.domain.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.*;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/timeslots")
 public class TimeSlotController {
-
-    private final UserServiceImpl userService;
-    private final PartnerServiceImpl partnerService;
     private final TimeSlotServiceImpl timeSlotService;
     private final ReservationServiceImpl reservationService;
     private final GeolocaliztionService geolocaliztionService;
 
-    public TimeSlotController(PartnerServiceImpl partnerService, UserServiceImpl userServiceImpl, TimeSlotServiceImpl timeSlotService, ReservationServiceImpl reservationServiceImpl, GeolocaliztionService geolocaliztionService) {
-        this.partnerService = partnerService;
-        this.userService = userServiceImpl;
-    public TimeSlotController(TimeSlotServiceImpl timeSlotService, ReservationServiceImpl reservationServiceImpl) {
+    public TimeSlotController(TimeSlotServiceImpl timeSlotService, ReservationServiceImpl reservationServiceImpl, GeolocaliztionService geolocaliztionService) {
         this.timeSlotService = timeSlotService;
         this.reservationService = reservationServiceImpl;
         this.geolocaliztionService = geolocaliztionService;
@@ -52,14 +43,12 @@ public class TimeSlotController {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "reservation already exists in this slot");
         }
 
-        Reservation created = reservationService.create(user);
+        Reservation created = reservationService.create(reservation);
         timeslot.get().setReservation(created);
         timeSlotService.update(timeslot.get());
 
         return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
-
-
 
     @GetMapping("test/{addr}")
     public GeoCoordsResponse test(@PathVariable String addr){
