b6318aa144ff345f8566de92ea8de4297501b14a
Juliien <julien1105@outlook.fr>
2021-07-06 11:12:04 +0200

    fix: authority

--- src/main/java/fr/esgi/jee/api/authentication/configuration/WebSecurityConfig.java
+++ src/main/java/fr/esgi/jee/api/authentication/configuration/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/welcome").permitAll()
-                .antMatchers(HttpMethod.GET, "/partner").permitAll()
+                .antMatchers(HttpMethod.POST, "/partner").hasAuthority("PARTNER")
                 .anyRequest()
                 .authenticated()
                 .and()
